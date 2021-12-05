0896406200b512d3cfa0e08c581b633b162511ab
PatrykJarzynka <patryk.jarzynka1234@gmail.com>
2021-12-05 13:23:58 +0100

    poprawa funkcji

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,13 @@
 const fetchCountries = name => {
-  fetch(`https://restcountries.com/v3.1/name/${name}`)
-    .then(resolve => resolve.json())
-    .then(resolve => console.log(resolve));
+    fetch(
+      `https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.svg,languages`
+    )
+      .then(resolve => {
+        if (!resolve.ok) throw new Error(resolve.status);
+        return resolve.json();
+      })
+
+      .then(resolve => console.log(resolve));
 };
 
 export { fetchCountries };
