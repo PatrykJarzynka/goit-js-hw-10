d98d8753ab609bdbc230bfe950558e6c0670d159
PatrykJarzynka <patryk.jarzynka1234@gmail.com>
2021-12-08 12:46:35 +0100

    Poprawiono wyświetlanie się informacji o państwach

--- src/index.js
+++ src/index.js
@@ -9,26 +9,26 @@ const list = document.querySelector('.country-list');
 const info = document.querySelector('.country-info');
 
 const placeResponse = elements => {
-  if (elements.length === 1) {
+  if (elements.length === 1)
+  {
     const markup = elements
       .map(element => {
         return `<li>
-                <p><img class="svg__img" src="${element.flags.svg}" alt="Flaga panstwa"> ${
-          element.name.common
-        } </p>
+                <p class="text"><img class="svg__img" src="${element.flags.svg}" alt="Flaga panstwa"> ${ element.name.common} </p>
                 <p> <b>Capital:</b> ${element.capital} </p>
                 <p> <b>Population:</b>${element.population} </p>
                 <p> <b>Languages:</b>${Object.values(element.languages)} </p>
             </li>`;
-      })
+        })
       .join('');
     info.innerHTML = markup;
     list.innerHTML = '';
-  } else if (elements.length >= 2 && elements.length <= 10) {
+  }
+  else if (elements.length >= 2 && elements.length <= 10) {
     const markup = elements
       .map(element => {
-        return `<li>
-                <p> ${element.name.common} </p>
+        return `<li class="library">
+                <p class="textSmall"> <img class="svg__img-small" src="${element.flags.svg}" alt="Flaga panstwa"> ${element.name.common} </p>
             </li>`;
       })
       .join(' ');
